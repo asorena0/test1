faeb21567138fe6bbd5f6a5d0c81b29ff9b22898
asorena0 <60604357+asorena0@users.noreply.github.com>
2020-10-17 12:38:22 +0330

    Add files via upload

---
+++ content.php
@@ -0,0 +1,159 @@
+<div class="acsUxWidget" style="margin-top: 10px;">
+    <div id="contentGrid_813691" class="acswidget acswidget-content-grid celwidget US bxw-content-grid bxw-content-grid--ember bxc-grid--padding bxc-grid--spacing-large  bxc-grid--light" cel_widget_id="acsux-widgets_content-grid_merchandised-search-3" data-is-mobile="false">
+        <script type="text/javascript">if (typeof uet == 'function') uet('bb', 'acsux-widgets_content-grid_merchandised-search-3', {wb: 1});  // timestamp body-begin</script>
+        <script type="text/javascript">if (typeof ue == 'function') {
+                ue.log({"widget":"contentGrid","type":"initialize"}, 'acsux-widgets', null);
+            }</script>
+
+
+
+        <div class="bxc-grid__container bxc-grid__container--width-1500">
+
+
+            <div class="bxc-grid__row bxc-grid--no-gutter   bxc-grid__row--light ">
+                <div class="bxc-grid__column  bxc-grid__column--12-of-12   bxc-grid__column--light">
+
+                    <div class="bxc-grid__content   bxc-grid__content--light celwidget" cel_widget_id="acsux-widgets_content-grid_merchandised-search-3_row1-col1">
+
+                        <div class="bxc-grid__image   bxc-grid__image--light">
+                            <a href="/gcx/Handmade-&amp;-Personalized-Gifts/gfhz/events/r/ref=s9_acss_bw_cg_hmhphol_1a1_w?categoryId=hgg19-hol-handmade-gifts&amp;pf_rd_m=ATVPDKIKX0DER&amp;pf_rd_s=merchandised-search-3&amp;pf_rd_r=Y51V7GJW5XWABGGWBDQP&amp;pf_rd_t=101&amp;pf_rd_p=d4cd6766-9758-409b-a8e0-beb52560cf2e&amp;pf_rd_i=11260432011" aria-label="Holiday décor and gift idea made by Makers">
+                                <img onload="window.uet &amp;&amp; uet.call &amp;&amp; uet(&quot;cf&quot;);" src="https://images-na.ssl-images-amazon.com/images/G/01/handmade/2020/Campaigns/Holiday/HM_HP_Header_Holiday_D.jpg" alt="Holiday décor and gift idea made by Makers">
+                            </a>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+
+        <script type="text/javascript">if (typeof uex == 'function') uex('ld', 'acsux-widgets_content-grid_merchandised-search-3', {wb: 1});  // timestamp page-loaded + send the metrics</script>
+        <script type="text/javascript">if (typeof uet == 'function') uet('be', 'acsux-widgets_content-grid_merchandised-search-3', {wb: 1});  // timestamp body-end</script>
+    </div>
+
+</div>
+
+<div class="container">
+    	<div class="container1">
+
+            <div>
+                <h1 style="text-align:center; margin-top: 20px;"> دسته بندی ها </h1>
+
+            </div>
+			<aside class="c-about-items">
+				<aside class="c-adplacement c-adplacement__container-row">
+					<a href="" class="c-adplacement__item js-banner-impression-adro" data-id="42568" target="_blank" data-is-trackable="" title="گزارش سال 1398 دیجی‌کالا">
+						<div class="c-adplacement__sponsored_box">
+							<img src="img/rings.jpg" alt="گزارش سال 1398 دیجی‌کالا"></div>
+					</a>
+					<a href="" class="c-adplacement__item js-banner-impression-adro" data-id="14789" target="_blank" data-is-trackable="" title="فروشنده شوید">
+						<div class="c-adplacement__sponsored_box">
+							<img src="img/earrings.jpg" alt="فروشنده شوید">
+						</div>
+					</a>
+					<a href="" class="c-adplacement__item js-banner-impression-adro" data-id="30914" target="_blank" data-is-trackable="" title="راضی به خرید !">
+						<div class="c-adplacement__sponsored_box">
+							<img src="img/necklace.jpg" alt="راضی به خرید !">
+						</div></a>
+					<a href="" class="c-adplacement__item js-banner-impression-adro" data-id="30723" target="_blank" data-is-trackable="" title="حرفه ای به خرید !">
+						<div class="c-adplacement__sponsored_box">
+							<img src="img/bracelet.jpg" alt="حرفه ای به خرید !">
+						</div>
+					</a>
+				</aside>
+			</aside>
+                     <aside class="c-about-items">
+				<aside class="c-adplacement c-adplacement__container-row">
+					<a href="https://www.digikala.com/mag/landing/report-1398/?&amp;promo_name=%DA%AF%D8%B2%D8%A7%D8%B1%D8%B4+%D8%B3%D8%A7%D9%84+1398+%D8%AF%DB%8C%D8%AC%DB%8C%E2%80%8C%DA%A9%D8%A7%D9%84%D8%A7&amp;promo_position=home_static_image&amp;promo_creative=42568&amp;bCode=42568" class="c-adplacement__item js-banner-impression-adro" data-id="42568" target="_blank" data-is-trackable="" title="گزارش سال 1398 دیجی‌کالا">
+						<div class="c-adplacement__sponsored_box">
+							<img src="img/decor.jpg" alt="گزارش سال 1398 دیجی‌کالا"></div>
+					</a>
+					<a href="https://www.digikala.com/landings/seller-introduction//?&amp;promo_name=%D9%81%D8%B1%D9%88%D8%B4%D9%86%D8%AF%D9%87+%D8%B4%D9%88%DB%8C%D8%AF&amp;promo_position=home_static_image&amp;promo_creative=14789&amp;bCode=14789" class="c-adplacement__item js-banner-impression-adro" data-id="14789" target="_blank" data-is-trackable="" title="فروشنده شوید">
+						<div class="c-adplacement__sponsored_box">
+							<img src="img/acc.jpg" alt="فروشنده شوید">
+						</div>
+					</a>
+					<a href="https://www.digikala.com/landings/cbr/?&amp;promo_name=%D8%B1%D8%A7%D8%B6%DB%8C+%D8%A8%D9%87+%D8%AE%D8%B1%DB%8C%D8%AF+%21&amp;promo_position=home_static_image&amp;promo_creative=30914&amp;bCode=30914" class="c-adplacement__item js-banner-impression-adro" data-id="30914" target="_blank" data-is-trackable="" title="راضی به خرید !">
+						<div class="c-adplacement__sponsored_box">
+							<img src="img/ppp.jpg" alt="راضی به خرید !">
+						</div></a>
+					<a href="https://www.digikala.com/landings/cbr-shipping/?&amp;promo_name=%D8%AD%D8%B1%D9%81%D9%87+%D8%A7%DB%8C+%D8%A8%D9%87+%D8%AE%D8%B1%DB%8C%D8%AF+%21&amp;promo_position=home_static_image&amp;promo_creative=30723&amp;bCode=30723" class="c-adplacement__item js-banner-impression-adro" data-id="30723" target="_blank" data-is-trackable="" title="حرفه ای به خرید !">
+						<div class="c-adplacement__sponsored_box">
+							<img src="img/bbgg.jpg" alt="حرفه ای به خرید !">
+						</div>
+					</a>
+				</aside>
+			</aside>  
+	</div>
+    <div>
+        <h1 style="text-align:center; margin-top: 20px;"> برترین ها </h1>
+
+    </div>
+    <section class="c-swiper c-swiper--products has-placeholder js-sntracker-carousel" id="recommendation-home_5">
+   <div class="c-box">
+
+      <div class="swiper-container swiper-container-horizontal swiper-container-rtl">
+         <div class="swiper-wrapper js-products-container">
+           
+        <div class="swiper-container swiper-container-horizontal swiper-container-rtl">
+                   <div class="swiper-wrapper js-products-container" style="transition-duration: 0ms; transform: translate3d(0px, 0px, 0px);">
+            	<?php
+                    $sql6="SELECT * FROM `products`";
+                    $counter=0;
+                    $res6=mysqli_query($conn,$sql6);
+                    $result7=mysqli_query($conn,$sql6);
+                    if(mysqli_num_rows($res6) > 0)
+                    {
+                        while(mysqli_fetch_assoc($res6))
+                        {
+                            $counter++;
+                        }
+                    }
+                    if($counter > 4)
+                    {
+                        for($i=$counter;$i>=$counter-5;$i--)
+                        {
+                            $sql6="SELECT * FROM `products` where id='$i'";
+                            $result7=mysqli_query($conn,$sql6);
+                            if($row=mysqli_fetch_assoc($result7))
+                            {
+                                echo '<div class="c-product-box">
+								      <img class="c-product-box__img js-url js-carousel-ga-product-box" src="'.$row['picture1'].'" >
+									  <div>
+									  	<a class="c-product-box__title" href="products.php?product='.$row['id'].'">'.$row['name'].'</a>
+										<h4 >'.$row['price'].'&nbsp;<small>تومان</small></h4>
+									  </div>
+									  </div>
+								';
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if(mysqli_num_rows($result7) > 0)
+                        {
+                            while($row=mysqli_fetch_assoc($result7))
+                            {
+                                echo '<div class="recent-product">
+										  <img src="'.$row['picture1'].'" width="150px" height="150px">
+										  <div>
+											<a href="products.php?product='.$row['id'].'">'.$row['name'].'</a>
+											<h4>'.$row['price'].'&nbsp;تومان</h4>
+										  </div>
+									  </div>
+								';
+							}
+                        }
+                    }
+                ?>
+					   			</div>
+
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+</div>
